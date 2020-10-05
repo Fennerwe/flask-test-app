ea80391a4d0406c9e91af58fbcdd49b482e492da
Fennerwe <fennerwe13@gmail.com>
2020-10-04 19:06:15 -0500

    Move files into packages

--- security.py
+++ security.py
@@ -1,12 +1,5 @@
-from user import User
+from resources.user import User
 from werkzeug.security import safe_str_cmp
-
-users = [
-    User(1, 'bob', 'asdf')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
