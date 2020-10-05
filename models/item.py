999670c03d1df47bee5ca3d2d79ec8316af9c4bd
Fennerwe <fennerwe13@gmail.com>
2020-10-04 21:59:38 -0500

    Use sqlalchemy in resource

--- models/item.py
+++ models/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import db
 
 
@@ -7,7 +6,7 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Float(preceision=2))
+    price = db.Column(db.Float(precision=2))
 
     def __init__(self, name, price):
         self.name = name
@@ -18,34 +17,12 @@ class ItemModel(db.Model):
 
     @classmethod
     def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(name=name).first()
 
-        query = 'SELECT * FROM items where name=?'
-        result = cursor.execute(query, (name,))
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return cls(*row)
-
-    def insert(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'INSERT INTO items VALUES (?, ?)'
-        cursor.execute(query, (self.name, self.price))
-
-        connection.commit()
-        connection.close()
-
-    def update(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'UPDATE items SET price=? WHERE name=?'
-        cursor.execute(query, (self.price, self.name))
-
-        connection.commit()
-        connection.close()
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
